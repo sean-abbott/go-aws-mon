f2d5c192c00858a449c4e6f73c1374b0549d4210
sabbott <sabbott@cargurus.com>
2017-08-24 16:41:02 -0400

    replace the rest of the accursed with spaces

--- round.go
+++ round.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"math"
+    "math"
 )
 
 func Round(f float64) float64 {
-	return math.Floor(f + .5)
+    return math.Floor(f + .5)
 }
 
 func RoundPlus(f float64, places int) float64 {
-	shift := math.Pow(10, float64(places))
-	return Round(f*shift) / shift
+    shift := math.Pow(10, float64(places))
+    return Round(f*shift) / shift
 }
